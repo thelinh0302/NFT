a1c104cedd6c7759d3a9ccd922f576790fa2db10
ext-linhchau <ext-linhchau@spdigital.sg>
2022-09-26 20:52:28 +0700

    completed implement BEP20 token

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: process.env.MNEMONIC}
+    },
+  }
 };
